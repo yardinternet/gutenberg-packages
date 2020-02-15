c7ab9bd6e18410428474007d0e1d772619019eee
Ferdie Sletering <ferdie@yard.nl>
2020-02-15 13:39:18 +0100

    (feat): add listControl, modal and listItem

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	collectCoverageFrom: [
+		'packages/**/src/**/*.js',
+		'!**/stories/**',
+		'!**/node_modules/**',
+		'!**/vendor/**',
+	],
+};
--- packages/google-maps/babel.config.js
+++ packages/google-maps/babel.config.js
@@ -2,6 +2,7 @@ module.exports = function( api ) {
 	api.cache( true );
 
 	return {
+		plugins: [ '@wordpress/babel-plugin-import-jsx-pragma' ],
 		presets: [ '@wordpress/babel-preset-default' ],
 	};
 };
