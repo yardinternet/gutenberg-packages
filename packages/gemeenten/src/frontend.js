cf85fdd9b7616d019ccf163ebb94c435092d08d1
Simon <simon@yard.nl>
2020-02-17 17:20:57 +0100

    (fix): remove pdcLocations from frontend.js

--- packages/gemeenten/src/frontend.js
+++ packages/gemeenten/src/frontend.js
@@ -1,10 +1,9 @@
 // This file loads up all the frontend components
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as PdcLocations from './blocks/pdc-locations/frontend';
 import * as PdcSearch from './blocks/pdc-search/frontend';
 
-const components = [ PdcLocations, PdcSearch ];
+const components = [ PdcSearch ];
 
 components.map( ( item ) => {
 	const element = document.getElementById( item.domID );
