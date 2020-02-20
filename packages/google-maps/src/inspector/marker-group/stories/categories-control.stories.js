453f680f53333c2a5b0a857083e55277ccd16be4
Ferdie Sletering <ferdie@yard.nl>
2020-02-20 17:19:58 +0100

    (feat): add categories control

---
+++ packages/google-maps/src/inspector/marker-group/stories/categories-control.stories.js
@@ -0,0 +1,34 @@
+import { action } from '@storybook/addon-actions';
+import { useState } from 'react';
+import { Button } from '@wordpress/components';
+import CategoriesControl from '../../categories-control';
+
+export default {
+	title: 'GoogleMaps/Inspector',
+};
+
+const defaultCategories = [
+	{ name: 'Category X' },
+	{ name: 'Category Y' },
+	{ name: 'Category Z' },
+];
+
+export const CategoriesControlC = () => {
+	const [ selectedCategories, setSelectedCategories ] = useState( [] );
+
+	const onChange = ( data ) => {
+		setSelectedCategories(
+			data.bool
+				? selectedCategories.concat( data.name )
+				: selectedCategories.filter( ( item ) => item !== data.name )
+		);
+	};
+
+	return (
+		<CategoriesControl
+			categories={ defaultCategories }
+			categoriesSelected={ selectedCategories }
+			onChange={ onChange }
+		/>
+	);
+};
