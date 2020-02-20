de9072f45abbeb1cc30f61aaffb3ba1213d6df1d
Ferdie Sletering <ferdie@yard.nl>
2020-02-20 17:19:58 +0100

    (feat): add categories control

---
+++ packages/google-maps/src/inspector/categories-control.js
@@ -0,0 +1,25 @@
+import { CheckboxControl } from '@wordpress/components';
+import { useState } from '@wordpress/element';
+
+function CategoriesControl( {
+	categories = [],
+	categoriesSelected = [],
+	onChange = () => {},
+} ) {
+	const isCategoryChecked = ( name ) => {
+		return categoriesSelected.includes( name );
+	};
+
+	return categories.map( ( { name }, index ) => {
+		return (
+			<CheckboxControl
+				key={ index }
+				label={ name }
+				checked={ isCategoryChecked( name ) }
+				onChange={ ( bool ) => onChange( { name, bool } ) }
+			/>
+		);
+	} );
+}
+
+export default CategoriesControl;
