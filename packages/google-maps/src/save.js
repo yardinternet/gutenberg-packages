6e49ca22dc5ff1444cf2b0dddac4153e7af0eefa
Ferdie Sletering <ferdie@yard.nl>
2020-02-24 17:14:41 +0100

    (feat): add frontend rendering

--- packages/google-maps/src/save.js
+++ packages/google-maps/src/save.js
@@ -1,17 +1,30 @@
-function save( { attributes } ) {
-	const x = {};
+import classnames from 'classnames';
+
+function save( { attributes, className } ) {
+	const props = {};
 	Object.keys( attributes ).map( ( item ) => {
 		if ( ! attributes[ item ] || attributes[ item ].length === 0 ) {
-			return;
+			return [];
 		}
 
-		return ( x[ 'data-' + item.toLowerCase() ] =
+		return ( props[ 'data-' + item.toLowerCase() ] =
 			typeof attributes[ item ] === 'string'
 				? attributes[ item ]
 				: JSON.stringify( attributes[ item ] ) );
 	} );
 
-	return <div id="gmap">Save</div>;
+	return (
+		<div
+			id="gmap"
+			className={ classnames( [
+				className,
+				'yard-google-map-advanced',
+			] ) }
+			{ ...props }
+		>
+			Save
+		</div>
+	);
 }
 
 export default save;
