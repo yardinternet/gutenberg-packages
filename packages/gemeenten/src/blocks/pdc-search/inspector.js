4f57ace3efcbf712983d9ad3b2c85cb5d7a28ea0
YvetteNikolov <yvettenikolov@gmail.com>
2020-03-11 13:53:42 +0100

    (feat): add option to add button text in stead of icon

--- packages/gemeenten/src/blocks/pdc-search/inspector.js
+++ packages/gemeenten/src/blocks/pdc-search/inspector.js
@@ -2,7 +2,7 @@
  * WordPress dependencies
  */
 import { InspectorControls } from '@wordpress/block-editor';
-import { PanelBody, TextControl } from '@wordpress/components';
+import { PanelBody, TextControl, CheckboxControl } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
 function Inspector( { attributes, setAttributes } ) {
@@ -10,16 +10,32 @@ function Inspector( { attributes, setAttributes } ) {
 		<InspectorControls>
 			<PanelBody title={ __( 'Opties' ) }>
 				<TextControl
-					label={ __( 'Label' ) }
+					label={ __( 'Input label' ) }
 					value={ attributes.label }
 					onChange={ ( value ) => setAttributes( { label: value } ) }
 				/>
 				<TextControl
-					label={ __( 'Zoek url' ) }
+					label={ __( 'Zoek URL' ) }
 					type="url"
 					value={ attributes.searchUrl }
 					onChange={ ( url ) => setAttributes( { searchUrl: url } ) }
 				/>
+				<CheckboxControl
+					label={ __( 'Tekst in plaats van icoon' ) }
+					value={ attributes.hasBtnText }
+					onChange={ ( checked ) =>
+						setAttributes( { hasBtnText: checked } )
+					}
+				/>
+				{ attributes.hasBtnText && (
+					<TextControl
+						label={ __( 'Knop label' ) }
+						value={ attributes.btnText }
+						onChange={ ( value ) =>
+							setAttributes( { btnText: value } )
+						}
+					/>
+				) }
 			</PanelBody>
 		</InspectorControls>
 	);
