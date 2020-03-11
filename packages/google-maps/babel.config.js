e27cb04faf537f06d98400bdd00b5a5d8df5eaf5
Mike van den Hoek <mvdhoek1991@gmail.com>
2020-03-11 15:20:08 +0100

    (fix): add babel jsx transform

--- packages/google-maps/babel.config.js
+++ packages/google-maps/babel.config.js
@@ -2,7 +2,10 @@ module.exports = function( api ) {
 	api.cache( true );
 
 	return {
-		plugins: [ '@wordpress/babel-plugin-import-jsx-pragma' ],
+		plugins: [
+			'@wordpress/babel-plugin-import-jsx-pragma',
+			'@babel/plugin-transform-react-jsx',
+		],
 		presets: [ '@wordpress/babel-preset-default' ],
 	};
 };
