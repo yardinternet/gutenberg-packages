19d973a8fc6e5d0c8f69bf975f49924f20722da8
Ferdie Sletering <ferdie@yard.nl>
2020-03-16 15:33:24 +0100

    (feat): add webpack-loaders package

---
+++ packages/webpack-loaders/src/index.js
@@ -0,0 +1,45 @@
+const isProduction = process.env.NODE_ENV === 'production';
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+const includePackages = require( '@yardinternet/webpack-include-packages' );
+
+/**
+ *
+ * @param {Array} packages
+ * @return {Object} config
+ */
+const gutenbergPackagesConfig = ( { packages = [] } ) => {
+	defaultConfig.module.rules[
+		isProduction ? 0 : 1
+	].exclude = includePackages( packages );
+	return {
+		...defaultConfig,
+		module: {
+			...defaultConfig.module,
+			rules: [
+				...defaultConfig.module.rules,
+				{
+					test: /\.css$/,
+					use: [ 'style-loader', 'css-loader' ],
+				},
+				{
+					test: /\.(woff|woff2|eot|ttf|otf|png|jpg|svg|gif)(\?v=\d+\.\d+\.\d+)?$/,
+					use: [
+						{
+							// With file loader which copies file
+							// and brings in URL to the file
+							loader: 'file-loader',
+							options: {
+								name: '[name]-[hash:6].[ext]',
+								outputPath: 'assets/',
+							},
+						},
+					],
+				},
+			],
+		},
+	};
+};
+
+module.exports = {
+	gutenbergPackagesConfig,
+};
