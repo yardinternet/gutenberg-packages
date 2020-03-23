f57ebf364f7e1dcdfaef86fa5ec17d2194ec5ddf
Ferdie Sletering <ferdie@yard.nl>
2020-03-23 13:07:53 +0100

    (refactor): marker-modal and textcontrol-focus-outside

--- packages/google-maps/src/config.js
+++ packages/google-maps/src/config.js
@@ -1,4 +1,18 @@
 export const config = {
 	textDomain: 'yardinternet-google-maps',
 	mapDomId: 'gmap',
+	infowindow: {
+		fields: {
+			url: {
+				label:
+					'URL voor in het informatievenster. Voorbeeld: https://www.domein.nl',
+			},
+			urlWindow: {
+				label: 'Link op andere pagina openen?',
+			},
+			content: {
+				label: 'Beschrijving voor in het informatievenster',
+			},
+		},
+	},
 };
