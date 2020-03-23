44ac3ba9eed840aec902556091b8528b104518d9
Ferdie Sletering <ferdie@yard.nl>
2020-03-23 21:32:18 +0100

    (feat): add category label to polygons inspector

--- packages/google-maps/src/inspector/list-control/list.js
+++ packages/google-maps/src/inspector/list-control/list.js
@@ -3,23 +3,49 @@ import { Button, Dashicon, BaseControl } from '@wordpress/components';
 function List( {
 	explanationNoItems,
 	data,
+	showCategoryLabel = false,
 	onRemove = () => {},
 	onModify = () => {},
 	children,
 } ) {
+	const renderCategory = ( index, item ) => {
+		if ( index === 0 ) {
+			return item.category;
+		}
+
+		if ( index > 0 && data[ index - 1 ].category !== item.category ) {
+			return item.category;
+		}
+		return null;
+	};
+
 	return (
 		<>
 			{ data && data.length > 0 ? (
 				<div style={ { width: '100%' } }>
-					{ data.map( ( item, index ) => (
-						<List.Item
-							key={ index }
-							item={ item.name }
-							index={ index }
-							onRemove={ onRemove }
-							onModify={ onModify }
-						/>
-					) ) }
+					{ data.map( ( item, index ) => {
+						return (
+							<div key={ index }>
+								{ showCategoryLabel && (
+									<div
+										style={ {
+											marginBottom: '10px',
+											fontWeight: 'bold',
+										} }
+									>
+										{ renderCategory( index, item ) }
+									</div>
+								) }
+								<List.Item
+									key={ index }
+									item={ item.name }
+									index={ index }
+									onRemove={ onRemove }
+									onModify={ onModify }
+								/>
+							</div>
+						);
+					} ) }
 				</div>
 			) : (
 				<span>{ explanationNoItems }</span>
