2be0817b924070172e52a4955ed37678ee450211
YvetteNikolov <yvettenikolov@gmail.com>
2020-08-04 10:46:21 +0200

    (fix): aria-expanded attribute on collapse item

--- packages/core/src/collapse/collapse-item/save.js
+++ packages/core/src/collapse/collapse-item/save.js
@@ -15,7 +15,7 @@ function save( props ) {
 					type={ `button` }
 					data-toggle={ `collapse` }
 					data-target={ `#collapse-${ id }` }
-					aria-expanded={ false }
+					aria-expanded="false"
 					aria-controls={ `collapse-${ id }` }
 				>
 					{ headerText }
