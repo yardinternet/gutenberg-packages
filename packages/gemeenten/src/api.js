1a8ecd786cc7d77c6cc9cb4c81ef8e13c5647b80
Mike van den Hoek <mvdhoek1991@gmail.com>
2020-11-30 18:15:39 +0100

    (fix): add basic auth to pub requests

--- packages/gemeenten/src/api.js
+++ packages/gemeenten/src/api.js
@@ -22,7 +22,9 @@ export async function searchOpenpub(
 
 async function fetchOWC( query ) {
 	try {
-		const response = await fetch( query );
+		const response = await fetch( query, {
+			headers: getBasicAuth(),
+		} );
 
 		if ( ! response.ok ) {
 			throw new Error( 'Error: could not fetch endpoint.' );
@@ -35,3 +37,21 @@ async function fetchOWC( query ) {
 		return false;
 	}
 }
+
+/**
+ * Return basic auth header when needed
+ */
+const getBasicAuth = () => {
+	const authUsername = theme.acceptAuthUsername ?? null;
+	const authPassword = theme.acceptAuthPassword ?? null;
+	const isProd = theme.appEnv === 'production';
+	const withBasicAuth = ! isProd && !! authUsername && !! authPassword;
+
+	if ( ! withBasicAuth ) {
+		return {};
+	}
+
+	return {
+		Authorization: 'Basic ' + btoa( `${ authUsername }:${ authPassword }` ),
+	};
+};
