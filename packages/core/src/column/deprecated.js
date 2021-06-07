87957397f6d02f7b1e9efd1bdeda5f8aa52766f5
YvetteNikolov <yvettenikolov@gmail.com>
2021-06-07 15:37:55 +0200

    (feat): write deprecated for attribute change of grid

--- packages/core/src/column/deprecated.js
+++ packages/core/src/column/deprecated.js
@@ -11,6 +11,39 @@ import ColumnDeprecated from './components/column-deprecated';
 import { attributes } from './settings';
 
 const deprecated = [
+	// since 1.2.6
+	{
+		attributes: {
+			editIsSelected: {
+				type: 'boolean',
+				default: false,
+			},
+			colClassLg: {
+				type: 'number',
+				default: 0,
+			},
+			colClass: {
+				type: 'number',
+				default: 6,
+			},
+			colClassSm: {
+				type: 'number',
+				default: 0,
+			},
+			colClassXs: {
+				type: 'number',
+				default: 0,
+			},
+		},
+
+		save: ( props ) => {
+			return (
+				<Column { ...props }>
+					<InnerBlocks.Content />
+				</Column>
+			);
+		},
+	},
 	// Deprecated version is used in EVB Plus
 	{
 		attributes,
