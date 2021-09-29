78e7878c378280577e18403baf29486c3821c32d
Lara van Diemen <laravandiemen@workstation05.intraxs.local>
2021-09-29 14:24:27 +0200

    (feat): add heading selection to collapse block

--- packages/core/src/collapse/edit.js
+++ packages/core/src/collapse/edit.js
@@ -4,8 +4,13 @@
 import { InnerBlocks } from '@wordpress/block-editor';
 import { useEffect } from '@wordpress/element';
 
+/**
+ * Internal dependencies
+ */
+import Inspector from './components/inspector';
+
 function Edit( { clientId, attributes, setAttributes } ) {
-	const { accordionId } = attributes;
+	const { accordionId, heading } = attributes;
 
 	const TEMPLATE = [ [ 'yard-blocks/collapse-item' ] ];
 	const ALLOWED_BLOCKS = [ 'yard-blocks/collapse-item' ];
@@ -15,18 +20,25 @@ function Edit( { clientId, attributes, setAttributes } ) {
 	}, [ clientId ] );
 
 	return (
-		<div
-			className={ `yard-blocks-collapse` }
-			id={ `accordion-${ accordionId }` }
-		>
-			<InnerBlocks
-				renderAppender={ () => <InnerBlocks.ButtonBlockAppender /> }
-				allowedBlocks={ ALLOWED_BLOCKS }
-				defaultBlock={ ALLOWED_BLOCKS }
-				template={ TEMPLATE }
-				templateLock={ false }
+		<>
+			<Inspector
+				heading={ heading }
+				setAttributes={ setAttributes }
+				clientId={ clientId }
 			/>
-		</div>
+			<div
+				className={ `yard-blocks-collapse` }
+				id={ `accordion-${ accordionId }` }
+			>
+				<InnerBlocks
+					renderAppender={ () => <InnerBlocks.ButtonBlockAppender /> }
+					allowedBlocks={ ALLOWED_BLOCKS }
+					defaultBlock={ ALLOWED_BLOCKS }
+					template={ TEMPLATE }
+					templateLock={ false }
+				/>
+			</div>
+		</>
 	);
 }
 
