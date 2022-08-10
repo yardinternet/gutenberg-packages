1819a29ebfb8d085e7b4b0c4ec721a9354ef385d
YvetteNikolov <yvettenikolov@gmail.com>
2022-08-10 13:44:15 +0200

    (feat): add separate exports to gemeente blocks for manual block registration

--- packages/gemeenten/src/index.js
+++ packages/gemeenten/src/index.js
@@ -3,7 +3,14 @@
  * Internal dependencies
  */
 import './unregister-blocks';
+import * as pdcGreeting from './blocks/pdc-greeting';
+import * as pdcSearch from './blocks/pdc-search';
+import * as pdcServicePoints from './blocks/pdc-servicepoints';
+import * as pubListPosts from './blocks/pub-listposts';
+import * as pubPoll from './blocks/pub-poll';
 
 export { SEARCH_ENDPOINT } from './config/endpoints';
 export * from './blocks/pdc-search';
 export { registerGemeentenBlocks } from './register-blocks';
+
+export { pdcGreeting, pdcSearch, pdcServicePoints, pubListPosts, pubPoll };
