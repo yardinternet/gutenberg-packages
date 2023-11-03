161c3fc805a758b3ba40fa6b5888fffd0e4bc21f
YvetteNikolov <yvettenikolov@gmail.com>
2023-11-03 14:30:57 +0100

    (chore): add aria-expanded deprecated function to collapse-item

--- packages/core/src/collapse/collapse-item/deprecated.js
+++ packages/core/src/collapse/collapse-item/deprecated.js
@@ -6,9 +6,120 @@ import parse from 'html-react-parser';
 /**
  * WordPress dependencies
  */
-import { InnerBlocks } from '@wordpress/block-editor';
+import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
 
 const deprecated = [
+	// Since fixing the aria-expanded attribute on the header button
+	{
+		attributes: {
+			headerText: {
+				type: 'string',
+				default: ' ',
+			},
+			showOpen: {
+				type: 'boolean',
+				default: false,
+			},
+			isAccordion: {
+				type: 'boolean',
+				default: true,
+			},
+			id: {
+				type: 'string',
+				default: '0',
+			},
+			parentClientId: {
+				type: 'string',
+				default: '0',
+			},
+			heading: {
+				type: 'string',
+				default: '',
+			},
+			structuredData: {
+				type: 'boolean',
+				default: false,
+			},
+		},
+		save( props ) {
+			const { attributes } = props;
+			const {
+				id,
+				headerText,
+				showOpen,
+				isAccordion,
+				parentClientId,
+				heading,
+				structuredData,
+			} = attributes;
+
+			const blockProps = useBlockProps.save( {
+				className: 'yard-blocks-collapse-item',
+			} );
+
+			const button = () => {
+				return `
+					<button
+						class="yard-blocks-collapse-item__button"
+						type="button"
+						data-toggle="collapse"
+						data-target="#collapse-${ id }"
+						aria-expanded="false"
+						aria-controls="collapse-${ id }"
+						${ structuredData ? 'itemprop="name"' : '' }
+					>
+						${ headerText }
+					</button>
+				`;
+			};
+
+			const header = () => {
+				if ( heading === '' ) return button();
+
+				return `
+					<${ heading } class="yard-blocks-collapse-item__heading">
+						${ button() }
+					</${ heading }>
+				`;
+			};
+
+			return (
+				<div
+					{ ...blockProps }
+					itemScope={ structuredData }
+					itemProp={ structuredData ? 'mainEntity' : null }
+					itemType={
+						structuredData ? 'https://schema.org/Question' : null
+					}
+				>
+					<div className="yard-blocks-collapse-item__header">
+						{ parse( header() ) }
+					</div>
+					<div
+						className={ `collapse ${ showOpen ? 'show' : '' }` }
+						id={ `collapse-${ id }` }
+						data-parent={
+							isAccordion
+								? `#accordion-${ parentClientId }`
+								: null
+						}
+						itemScope={ structuredData }
+						itemProp={ structuredData ? 'acceptedAnswer' : null }
+						itemType={
+							structuredData ? 'https://schema.org/Answer' : null
+						}
+					>
+						<div
+							className="yard-blocks-collapse-item__body"
+							itemProp={ structuredData ? 'text' : null }
+						>
+							<InnerBlocks.Content />
+						</div>
+					</div>
+				</div>
+			);
+		},
+	},
 	// Since adding blockprops
 	{
 		attributes: {
