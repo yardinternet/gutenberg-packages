7219272756d9201cf8d3deaa2eca0a64cd9593fb
YvetteNikolov <yvettenikolov@gmail.com>
2024-04-03 11:38:52 +0200

    (chore): add hooks and components packages

---
+++ packages/components/src/block-icon-color/index.js
@@ -0,0 +1,8 @@
+/**
+ * Internal dependencies
+ */
+import './editor.scss';
+
+export const BlockIconColor = {
+	foreground: '#00a49e',
+};
---
+++ packages/components/src/icon/index.js
@@ -0,0 +1,13 @@
+export const Icon = ( props ) => {
+	const { attributes } = props;
+	const { icon, iconAltText, iconColor } = attributes;
+
+	return (
+		<i
+			className={ `wp-block-yard-icon-component fa-fw ${ icon } ` }
+			title={ iconAltText ? iconAltText : null }
+			aria-hidden="true"
+			style={ { color: iconColor } }
+		></i>
+	);
+};
