a3179ba6a8827588255f197762a54e765ddbcf2e
YvetteNikolov <yvettenikolov@gmail.com>
2024-04-04 11:29:38 +0200

    (chore): add packages to config function to webpack-loaders

---
+++ packages/webpack-loaders/src/deprecated/index.js
@@ -0,0 +1,52 @@
+/**
+ * WordPress dependencies
+ */
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+
+/**
+ * External dependencies
+ */
+const includePackages = require( '@yardinternet/webpack-include-packages' );
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+/**
+ * Webpack loader that extends defaultConfig of @wordpress/scripts
+ * Supports css, fonts, images
+ *
+ * @param {Array} packages
+ * @return {Object} config
+ */
+const gutenbergPackagesConfig = ( { packages = [] } ) => {
+	defaultConfig.module.rules[ isProduction ? 0 : 1 ].exclude =
+		includePackages( packages );
+	return {
+		...defaultConfig,
+		module: {
+			...defaultConfig.module,
+			rules: [
+				...defaultConfig.module.rules,
+				{
+					test: /\.css$/,
+					use: [ 'style-loader', 'css-loader' ],
+				},
+				{
+					test: /\.(woff|woff2|eot|ttf|otf|png|jpg|svg|gif)(\?v=\d+\.\d+\.\d+)?$/,
+					use: [
+						{
+							// With file loader which copies file
+							// and brings in URL to the file
+							loader: 'file-loader',
+							options: {
+								name: '[name]-[hash:6].[ext]',
+								outputPath: 'assets/',
+							},
+						},
+					],
+				},
+			],
+		},
+	};
+};
+
+module.exports = gutenbergPackagesConfig;
