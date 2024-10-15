8a25d92cdfd1ad1c730f4ec848bbf931cf31356e
Lara van Diemen <lara@yard.nl>
2024-10-15 09:52:59 +0200

    (fix): remove color form icon component

--- packages/components/src/icon/index.js
+++ packages/components/src/icon/index.js
@@ -1,13 +1,12 @@
 export const Icon = ( props ) => {
 	const { attributes } = props;
-	const { icon, iconAltText, iconColor } = attributes;
+	const { icon, iconAltText } = attributes;
 
 	return (
 		<i
 			className={ `wp-block-yard-icon-component fa-fw ${ icon } ` }
 			title={ iconAltText ? iconAltText : null }
 			aria-hidden="true"
-			style={ { color: iconColor } }
 		></i>
 	);
 };
