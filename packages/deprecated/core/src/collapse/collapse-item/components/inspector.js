e3837b7d5ca8a705c51e6da24b77841b6d07ea8d
Wybe van den Bosch <wybe.vandenbosch+git@gmail.com>
2024-11-08 16:46:11 +0100

    (fix): use native link block

--- packages/deprecated/core/src/collapse/collapse-item/components/inspector.js
+++ packages/deprecated/core/src/collapse/collapse-item/components/inspector.js
@@ -3,34 +3,13 @@
  */
 import { InspectorControls } from '@wordpress/block-editor';
 import { applyFilters } from '@wordpress/hooks';
-import { PanelBody, ToggleControl, TextControl } from '@wordpress/components';
+import {
+	PanelBody,
+	ToggleControl,
+	TextControl,
+	ExternalLink,
+} from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-
-function CustomHelp() {
-	return (
-		<span className="components-base-control__help">
-			Typ één of twee woorden om een uniek webadres te maken voor dit
-			blok, genaamd een “anker.” Daarna kun je direct naar deze sectie op
-			je pagina linken.
-			<a
-				className="components-external-link"
-				href="https://wordpress.org/documentation/article/page-jumps/"
-				target="_blank"
-				rel="external noreferrer noopener"
-			>
-				<span className="components-external-link__contents">
-					Meer informatie over ankers
-				</span>
-				<span
-					className="components-external-link__icon"
-					aria-label="(opent in een nieuwe tab)"
-				>
-					<span>↗</span>
-				</span>
-			</a>
-		</span>
-	);
-}
 
 function Inspector( {
 	showOpen = false,
@@ -75,7 +54,22 @@ function Inspector( {
 						label={ __( 'Handmatige naam' ) }
 						value={ anchorName }
 						onChange={ sanatizeAnchorName }
-						help={ <CustomHelp /> }
+						help={
+							<>
+								{ __(
+									'Typ één of twee woorden — zonder spaties — om een uniek webadres te maken voor dit blok, genaamd een “anker.” Daarna kun je direct naar deze sectie op je pagina linken.'
+								) }
+								<ExternalLink
+									href={ __(
+										'https://wordpress.org/documentation/article/page-jumps/'
+									) }
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									{ __( 'Meer informatie over ankers' ) }
+								</ExternalLink>
+							</>
+						}
 					/>
 				</PanelBody>
 			) }
