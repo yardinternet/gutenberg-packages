f8d78666504b386eea62c6c563b560121ed62ff9
Dik Takken <github@d.h.j.takken.freedom.nl>
2024-12-02 13:49:40 +0100

    Mark Tolkie blocks as preferred for button placement

--- packages/deprecated/core/src/tolkie/index.js
+++ packages/deprecated/core/src/tolkie/index.js
@@ -32,7 +32,7 @@ export const settings = {
 	attributes,
 	save: () => {
 		return (
-			<div className="yard-blocks-tolkie | tolkie-buttons-afterend"></div>
+			<div className="yard-blocks-tolkie | tolkie-buttons-afterend | tolkie-preferred"></div>
 		);
 	},
 };
