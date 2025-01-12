425017eb5363a9642362b763d9585b8c975e11cd
Hugo Cox <h.n.c.cox@outlook.com>
2025-01-12 16:16:48 +0100

    (feat): add greetings component

---
+++ packages/deprecated/gemeenten/src/blocks/greetings/components/greetings.js
@@ -0,0 +1,26 @@
+function Greetings( props ) {
+    const { attributes, date = new Date(), defaultGreeting = '' } = props;
+    const { greetings } = attributes;
+
+    const getLabel = ( hour, minutes ) => {
+        const currentTime = hour * 100 + minutes;
+        for (const key in greetings) {
+            const [start, end] = key.split('-').map(time => parseInt(time.replace(':', ''), 10));
+            if (currentTime >= start && currentTime <= end) {
+                return greetings[key];
+            }
+        }
+
+        return defaultGreeting;
+    };
+
+    const label = getLabel( date.getHours(), date.getMinutes() );
+
+	return (
+		<div className="gemeenten-block gemeenten-block--greetings">
+			{ label }
+		</div>
+	);
+}
+
+export default Greetings;
