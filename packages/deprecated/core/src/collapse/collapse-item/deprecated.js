372fbb8477779f78b6becf6b999e067016a7f875
Freek de Kruijff <freek.dekruijff@yard.nl>
2025-02-28 17:08:27 +0100

    (chore): create deprecated version

--- packages/deprecated/core/src/collapse/collapse-item/deprecated.js
+++ packages/deprecated/core/src/collapse/collapse-item/deprecated.js
@@ -9,6 +9,117 @@ import parse from 'html-react-parser';
 import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
 
 const deprecated = [
+	// Since subtitle option
+	{
+		attributes: {
+			headerText: {
+				type: "string",
+				default: " "
+			},
+			showOpen: {
+				type: "boolean",
+				default: false
+			},
+			isAccordion: {
+				type: "boolean",
+				default: true
+			},
+			id: {
+				type: "string",
+				default: "0"
+			},
+			parentClientId: {
+				type: "string",
+				default: "0"
+			},
+			heading: {
+				type: "string",
+				default: ""
+			},
+			anchorName: {
+				type: "string",
+				default: ""
+			},
+			structuredData: {
+				type: "boolean",
+				default: false
+			}
+		},
+		save( props ) {
+			const { attributes } = props;
+			const {
+				id,
+				headerText,
+				showOpen,
+				isAccordion,
+				parentClientId,
+				heading,
+				structuredData,
+				anchorName,
+			} = attributes;
+
+			const blockProps = useBlockProps.save( {
+				className: 'yard-blocks-collapse-item',
+			} );
+
+			const button = () => {
+				return `
+			<button
+				class="yard-blocks-collapse-item__button"
+				type="button"
+				${ anchorName ? `data-anchor-name="${ anchorName }"` : '' }
+				data-toggle="collapse"
+				data-target="#collapse-${ id }"
+				aria-expanded="${ showOpen ? 'true' : 'false' }"
+				aria-controls="collapse-${ id }"
+				${ structuredData ? 'itemprop="name"' : '' }
+			>
+				${ headerText }
+			</button>
+		`;
+			};
+
+			const header = () => {
+				if ( heading === '' ) return button();
+
+				return `
+			<${ heading } class="yard-blocks-collapse-item__heading">
+				${ button() }
+			</${ heading }>
+		`;
+			};
+
+			return (
+				<div
+					{ ...blockProps }
+					itemScope={ structuredData }
+					itemProp={ structuredData ? 'mainEntity' : null }
+					itemType={ structuredData ? 'https://schema.org/Question' : null }
+				>
+					<div className="yard-blocks-collapse-item__header">
+						{ parse( header() ) }
+					</div>
+					<div
+						className={ `collapse ${ showOpen ? 'show' : '' }` }
+						id={ `collapse-${ id }` }
+						data-parent={
+							isAccordion ? `#accordion-${ parentClientId }` : null
+						}
+						itemScope={ structuredData }
+						itemProp={ structuredData ? 'acceptedAnswer' : null }
+						itemType={ structuredData ? 'https://schema.org/Answer' : null }
+					>
+						<div
+							className="yard-blocks-collapse-item__body"
+							itemProp={ structuredData ? 'text' : null }
+						>
+							<InnerBlocks.Content />
+						</div>
+					</div>
+				</div>
+			);
+		}
+	},
 	// Since fixing the aria-expanded attribute on the header button
 	{
 		attributes: {
