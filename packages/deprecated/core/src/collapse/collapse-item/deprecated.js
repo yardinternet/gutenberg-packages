c056ae28e16ebb0ecc6eff860d1fa229f5ef0091
rivanuff <rivanuff@gmail.com>
2026-04-01 15:17:48 +0200

    fix: strict check for collapse item heading attribute

--- packages/deprecated/core/src/collapse/collapse-item/deprecated.js
+++ packages/deprecated/core/src/collapse/collapse-item/deprecated.js
@@ -9,6 +9,128 @@ import parse from 'html-react-parser';
 import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
 
 const deprecated = [
+	// Fix for failed block validation due to heading being undefined
+	{
+		attributes: {
+			headerText: {
+				type: 'string',
+				default: ' ',
+			},
+			showOpen: {
+				type: 'boolean',
+				default: false,
+			},
+			isAccordion: {
+				type: 'boolean',
+				default: true,
+			},
+			id: {
+				type: 'string',
+				default: '0',
+			},
+			parentClientId: {
+				type: 'string',
+				default: '0',
+			},
+			heading: {
+				type: 'string',
+				default: '',
+			},
+			anchorName: {
+				type: 'string',
+				default: '',
+			},
+			structuredData: {
+				type: 'boolean',
+				default: false,
+			},
+		},
+		save( props ) {
+			const { attributes } = props;
+			const {
+				id,
+				headerText,
+				showOpen,
+				isAccordion,
+				parentClientId,
+				heading,
+				structuredData,
+				anchorName,
+			} = attributes;
+
+			const blockProps = useBlockProps.save( {
+				className: 'yard-blocks-collapse-item',
+			} );
+
+			const button = () => {
+				return `
+            <button
+                class="yard-blocks-collapse-item__button"
+                type="button"
+                ${ anchorName ? `data-anchor-name="${ anchorName }"` : '' }
+                data-toggle="collapse"
+                data-target="#collapse-${ id }"
+                aria-expanded="${ showOpen ? 'true' : 'false' }"
+                aria-controls="collapse-${ id }"
+                ${ structuredData ? 'itemprop="name"' : '' }
+            >
+                ${ headerText }
+            </button>
+        `;
+			};
+
+			const header = () => {
+				if (
+					heading === '' ||
+					heading === null ||
+					heading === undefined
+				)
+					return button();
+
+				return `
+            <${ heading } class="yard-blocks-collapse-item__heading">
+                ${ button() }
+            </${ heading }>
+        `;
+			};
+
+			return (
+				<div
+					{ ...blockProps }
+					itemScope={ structuredData }
+					itemProp={ structuredData ? 'mainEntity' : null }
+					itemType={
+						structuredData ? 'https://schema.org/Question' : null
+					}
+				>
+					<div className="yard-blocks-collapse-item__header">
+						{ parse( header() ) }
+					</div>
+					<div
+						className={ `collapse ${ showOpen ? 'show' : '' }` }
+						id={ `collapse-${ id }` }
+						data-parent={
+							isAccordion
+								? `#accordion-${ parentClientId }`
+								: null
+						}
+						itemScope={ structuredData }
+						itemProp={ structuredData ? 'acceptedAnswer' : null }
+						itemType={
+							structuredData ? 'https://schema.org/Answer' : null
+						}
+					>
+						<div
+							className="yard-blocks-collapse-item__body"
+							itemProp={ structuredData ? 'text' : null }
+						>
+							<InnerBlocks.Content />
+						</div>
+					</div>
+				</div>
+			);
+		},
+	},
 	// Since subtitle option
 	{
 		attributes: {
